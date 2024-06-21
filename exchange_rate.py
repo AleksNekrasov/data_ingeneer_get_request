f082b0251cb1ad055c373bd0a452491801cd545b
AleksNekrasov <nsk540010@gmail.com>
2024-06-21 22:37:02 +0700

    доработать немного

---
+++ exchange_rate.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+#  Example Request: https://v6.exchangerate-api.com/v6/79e8ba26d7e58f3e988dcaa0/latest/USD
+
+
+key = f'79e8ba26d7e58f3e988dcaa0'
+
+URL = f'https://v6.exchangerate-api.com/v6/{key}/latest/USD'
+
+get_ex = f"https://v6.exchangerate-api.com/v6/{key}/pair/USD/RUB"
+
+probe = f"https://v6.exchangerate-api.com/v6/{key}/pair/USD/RUB/AMOUNT"
+
+r = requests.get(url=URL)
+
+if r.status_code == 200:
+    data = r.json()        # преобразуем json в вид словаря
+    result = json.dumps(data, indent=4, ensure_ascii=False) # преобразуем в стринг
+
+    list_valutes = data.get("conversion_rates").keys()
+    print(result)
+    print(f'Data type is: {(type(data))}')
+    print(f'Result type is: {(type(result))}')
+    print(list_valutes)
+    print(type(list_valutes))
+
+
+
+
+#result = r.json()
+#print(result)
+#print(json.dumps(result, indent=4, ensure_ascii=False))
+print("------------------------------------------------------")
+#f = requests.get(url=probe)
+#resulter = f.json()
+#print(json.dumps(resulter, indent=4, ensure_ascii=False))
+#print(result.get('conversion_rates').get("RUB"))
--- post_get.py
+++ post_get.py
@@ -1,13 +1,15 @@
 import requests
 # https://apilayer.com/marketplace/exchangerates_data-api#documentation-tab - api площадка
 import _json
-
+URL = f'https://api.apilayer.com/currency_data/convert?base=USD&symbols=EUR,GBP,JPY&amount=5&date=2024-01-01'
 
 api_key = "pbIabQqVSqpTp9yNi9ln7ULzSV0y8SDR"
 
-#curl --request GET 'https://api.apilayer.com/currency_data/convert?base=USD&symbols=EUR,GBP,JPY&amount=5&date=2018-01-01' \
-#--header 'apikey: YOUR API KEY'
+header = {'apikey': api_key}
 
-r = requests.get(url=url)
+#curl --request GET 'https://api.apilayer.com/currency_data/convert?base=USD&symbols=EUR,GBP,JPY&amount=5&date=2018-01-01' \
+#--header 'apikey: api_key
+
+r = requests.get(url=URL, headers=header)
 result = r.json()
 print(result)
